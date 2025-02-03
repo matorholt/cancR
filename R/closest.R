67a1f65b2fe1bc8d53309f05dc71167bf9768246
Mathias Ørholt <mathias.orholt@gmail.com>
2025-02-03 15:47:35 +0100

    Updates and examples

--- R/closest.R
+++ R/closest.R
@@ -5,22 +5,32 @@
 #'
 #'
 #' @param mdose estimated dose in the patient
+#' @param vec vector to search for closest value. If NULL, provide str which vec should be created from
 #' @param str strength of the tablet
 #' @param split whether the tablet can be split
+#'
 #'
 #' @return Returns the dose closest to the estimated
 #' @export
 #'
+#'  @examples
+#'  closest(25, str=50, split=F)
+#'  closest(50, c(100,200,300))
 #'
-closest <- function(mdose, str, split=T) {
+#'
+
+closest <- function(mdose, vec=NULL, str, split=T) {
   if(is.na(mdose)) {
     return(mdose)
   } else {
+    if(is.null(vec)){
     if(split) {
       vec <- str * c(0.5, seq(1,6))
     } else {
       vec <- str * seq(1, 6)
     }
+    }
   }
   vec[which.min(abs(mdose-vec))]
 }
+
--- R/map_drugs.R
+++ R/map_drugs.R
@@ -6,6 +6,7 @@
 #'
 #' @param data Dataframe, typically LMDB
 #' @param drug Regex filtering drugs of interest (e.g. L04AD01 or L0[34])
+#' @param windowsize Interval between two redeems that should be considered one
 #' @param pnr pnr
 #' @param atc atc
 #' @param eksd eksd
@@ -17,9 +18,48 @@
 #' @return A dataframe with dose, duration, cumulated dose as time-varying measures.
 #' @export
 #'
+#'  @examples
+#'  lmdb <- rbind(data.frame(pnr=c(1,1,1,1,1,1,1,1,1,1,1,2),
+#'                   atc=rep("L04AD01", 12),
+#'                   eksd = as.Date(c("2001-01-01",
+#'                                  "2001-04-01",
+#'                                  "2001-07-01",
+#'                                  "2001-10-01",
+#'                                  "2001-10-14",
+#'                                  "2001-12-31",
+#'                                  "2002-02-01",
+#'                                  "2002-02-14",
+#'                                  "2002-05-01",
+#'                                  "2002-05-14",
+#'                                  "2003-10-01",
+#'                                  "2001-01-01")),
+#'                 apk = c(2,2,2,1,1, 2, 1, 1,2,1,1,1),
+#'                 strnum = c(50, 50, 50, 100, 100, 50, 50,100, 50, 100, 100,50),
+#'                 packsize = c(50, 50, 50, 50, 50, 50, 50, 50, 50,50, 50,50)),
+#'
+#'            data.frame(pnr=c(3),
+#'                       atc=rep("L04AD01", 1),
+#'                       eksd = as.Date(c("2001-01-01")),
+#'                       apk = c(6),
+#'                       strnum = c(50),
+#'                       packsize = c(50)),
+#'            data.frame(pnr=rep(4, 3),
+#'                       atc=rep("L04AD01", 3),
+#'                       eksd = as.Date(c("2001-01-01",
+#'                                        "2001-01-11",
+#'                                        "2001-01-25")),
+#'                       apk = c(3,3,2),
+#'                       strnum = c(50,25,50),
+#'                       packsize = c(50,50,50)))
+#'
+#' map_drugs(lmdb, drug = "L04AD01")
+
+#'
 #'
 map_drugs <- function(data,
                       drug,
+                      windowsize = 30,
+                      doses=NULL,
                       pnr = pnr,
                       atc = atc,
                       eksd = eksd,
@@ -27,11 +67,13 @@ map_drugs <- function(data,
                       strnum = strnum,
                       packsize = packsize,
                       split=T) {
-  lmdb %>%
+  data %>%
     filter(str_detect({{atc}}, drug)) %>%
+    group_by(pnr) %>%
+    arrange(pnr, eksd) %>%
     mutate(window = row_number(),
            #Make windows of prescriptions with short intervals
-           window = ifelse({{eksd}}- lag({{eksd}}) < 30 & row_number() > 1, NA, window)) %>%
+           window = ifelse({{eksd}}- lag({{eksd}}) < windowsize & row_number() > 1, NA, window)) %>%
     fill(window, .direction = "down") %>%
     group_by({{pnr}}, window) %>%
     #Collapse short invervals to one
@@ -52,19 +94,21 @@ map_drugs <- function(data,
     group_by({{pnr}}) %>%
     #Find total doses and durations
     mutate(total = str*packs*num,
-           mdose = total / as.numeric((lead({{eksd}}) - {{eksd}}))) %>%
+           mdose = total / as.numeric((lead({{eksd}}) - {{eksd}})),
+           mdose = ifelse(is.na(mdose), str, mdose)) %>%
     rowwise() %>%
     # Find closest dose among defined doses
-    mutate(dose = sapply(mdose, closest, str=str, split=split)) %>%
+    mutate(dose = sapply(mdose, closest, str=str, vec=doses, split=split)) %>%
     group_by({{pnr}}) %>%
     #
     mutate(dose = ifelse(row_number() == n(), mode(dose[str == last(str)]), dose),
            duration = pmin(as.numeric(lead({{eksd}}) - {{eksd}}), total/dose),
-           duration = ifelse(row_number() == n(), total/dose, duration)) %>%
-    #Time-varying stucture
-    mutate(tstart = as.numeric({{eksd}} - first({{eksd}})),
-           tstop = tstart + duration,
-           totaldose = duration *dose,
-           cumdose = cumsum(totaldose)) %>%
-    select({{pnr}}, {{atc}}, dose, duration, tstart, tstop, totaldose, cumdose)
+           duration = ifelse(row_number() == n(), total/dose, duration))
+    # #Time-varying stucture
+    # mutate(tstart = as.numeric({{eksd}} - first({{eksd}})),
+    #        tstop = tstart + duration,
+    #        totaldose = duration *dose,
+    #        cumdose = cumsum(totaldose)) %>%
+    # select({{pnr}}, {{atc}}, dose, duration, tstart, tstop, totaldose, cumdose)
 }
+
