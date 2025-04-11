ff3c8bc19068f7da31a33d2889923be2b717b4dd
Mathias Ørholt <mathias.orholt@gmail.com>
2025-04-11 11:57:53 +0200

    cprfix now handles and returns NAs

--- R/cprfix.R
+++ R/cprfix.R
@@ -11,17 +11,19 @@
 #' @return Returns same dataset with correct CPR numbers and optionally age and date of birth. Invalid CPRs stops the function and returns the invalid CPRs as a vector.
 #' @export
 #'
-#'
-#'
+
+
 cprfix <- function(data, cpr=cpr, extract=F) {
 
-  if(any(str_detect(data %>% pull({{cpr}}), "^\\d{9,10}$|^\\d{5,6}-?\\w{4}", negate=T) |
+  if(any(str_detect(data %>% pull({{cpr}}), "^\\d{9,10}$|^\\d{5,6}-?\\w{4}$", negate=T) |
      str_sub(data %>% pull({{cpr}}), 1,2) %in% c("00", seq(32,99)) |
-     str_sub(data %>% pull({{cpr}}), 3,4) %in% c("00", seq(13,99)))
+     str_sub(data %>% pull({{cpr}}), 3,4) %in% c("00", seq(13,99)) |
+     is.na(data %>% pull({{cpr}})))
                 ) {
-    errors <- data %>% filter(str_detect(data %>% pull({{cpr}}), "^\\d{9,10}$|^\\d{5,6}-?\\w{4}", negate=T) |
+    errors <- data %>% filter(str_detect(data %>% pull({{cpr}}), "^\\d{9,10}$|^\\d{5,6}-?\\w{4}$", negate=T) |
                       str_sub(data %>% pull({{cpr}}), 1,2) %in% c("00", seq(32,99)) |
-                      str_sub(data %>% pull({{cpr}}), 3,4) %in% c("00", seq(13,99)))
+                      str_sub(data %>% pull({{cpr}}), 3,4) %in% c("00", seq(13,99)) |
+                        is.na(data %>% pull({{cpr}})))
     warning(paste0(nrow(errors), " invalid CPR", rep("s", nrow(errors)>1), " detected and returned as vector"))
     return(errors %>% pull({{cpr}}))
 
@@ -46,5 +48,7 @@ cprfix <- function(data, cpr=cpr, extract=F) {
 
   return(data)
 }
+
+
 
 
