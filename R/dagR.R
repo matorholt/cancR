5c730dc878bf9acf56bd8327a02b6b455ee7437d
Mathias Ørholt <mathias.orholt@gmail.com>
2026-01-12 22:28:14 +0100

    dagR update

--- R/dagR.R
+++ R/dagR.R
@@ -6,55 +6,95 @@
 #'
 #' @param treatment name of treatment variable
 #' @param outcome name of outcome variable
-#' @param path.list named list of paths to draw in the format list("treatment" = list("to" = "outcome"), "confounder" = list("to" = c("treatment", "outcome")))
-#' @param arrow.fill color of the arrow heads
+#' @param path.list named list of paths to draw in the format list("treatment" = "outcome", "confounder" = c("treatment", "outcome"))
+#' @param label.fill fill color for labels
+#' @param label.color border.color for labels
+#' @param label.positions manual adjustment of label positions (x,y) in the format list("treatment" = c(0,0))
+#' @param treatment.fill fill color for the treatment label
+#' @param outcome.fill fill color for the outcome label
+#' @param arrow.fill fill color of the arrow heads
 #' @param arrow.color color of the arrows
-#' @param arrow.width linewidth of the arrows
-#' @param arrow.distance percentage reduction in arrow length
-#' @param curvature curvature of the arrows
-#' @param label.size text size of the variable nodes
+#' @param arrow.linewidth linewidth of the arrows
+#' @param arrow.size size of the arrow head in mm
+#' @param arrow.distance distance from label center to arrowhead as a fraction of the euclidian distance
+#' @param curvature named list of segments that should be curved in the format list(c("treatment", "outcome", 0.2))
+#' @param label.size text size of the labels
 #' @param dagitty output from dagitty.net inserted in single quotation marks.
+#' @param seed for reproducibility
 #'
 #' @returns plot of the specified dag
 #' @export
 #'
+#' @examples
+#' dagR(treatment = "treatment",
+#' outcome = "outcome",
+#' list("treatment" = "outcome",
+#'      "conf" = c("treatment", "outcome"),
+#'      "x1" = "outcome",
+#'      "x2" = c("treatment", "outcome")),
+#' arrow.distance = 0.25,
+#' arrow.linewidth = 0.7,
+#' label.positions = list("treatment" = c(5,5)),
+#' seed = 3,
+#' curvature = list(c("conf", "outcome", 0.2)))
+#'
+
+# dagR(dagitty = 'dag {
+# A [selected,pos="-2.155,-1.572"]
+# B [pos="1.311,-1.479"]
+# D [outcome,pos="1.312,1.327"]
+# E [exposure,pos="-2.084,1.383"]
+# Z [adjusted,pos="-0.466,-0.243"]
+# A -> Z [pos="-0.791,-1.045"]
+# A <-> E
+# B -> D
+# B -> Z [pos="0.680,-0.496"]
+# E -> D
+# }
+#
+# ')
 
 # dagR(treatment = "treatment",
 #      outcome = "outcome",
-#      list("treatment" = list("to" = "outcome"),
-#           "conf" = list("to" = c("treatment", "outcome")),
-#           "x1" = list("to" = c("outcome"))),
-#      arrow.distance = 0.05)
-#
-# dagR(dagitty = 'dag {
-#  C [pos="0.370,0.367"]
-#  Ex [pos="0.147,0.282"]
-#  Out [pos="0.558,0.266"]
-#  C -> Ex
-#  C -> Out
-#  Ex -> Out
-#  }')
+#      list("treatment" = "outcome",
+#           "conf" = c("treatment", "outcome"),
+#           "x1" = "outcome",
+#           "x2" = c("treatment", "outcome")),
+#      arrow.distance = 0.25,
+#      arrow.linewidth = 0.7,
+#      label.positions = list("treatment" = c(5,5)),
+#      seed = 3,
+#      curvature = list(c("conf", "outcome", 0.2)))
 
 dagR <- function(treatment,
                  outcome,
                  path.list,
-                 label.fill = "White",
-                 label.color = "White",
+                 label.fill = "#C4C4C4",
+                 label.color = "Black",
+                 label.positions = NULL,
+                 treatment.fill = "#AB5CB8",
+                 outcome.fill = "#97AFCC",
                  arrow.fill = "Black",
                  arrow.color = "Black",
-                 arrow.width = 0.7,
+                 arrow.linewidth = 0.7,
+                 arrow.size = 5,
                  arrow.distance = 0.2,
-                 curvature = 0,
+                 curvature = NULL,
                  label.size = 6,
-                 dagitty = NULL) {
+                 dagitty = NULL,
+                 seed=1) {
 
   if(!is.null(dagitty)) {
 
+    treatment <- str_extract(daggity, "\\w+(?=(\\s\\[exposure))")
+    outcome <- str_extract(daggity, "\\w+(?=(\\s\\[outcome))")
+
     vars <- unlist(str_extract_all(dagitty, "(?<!(\\>\\s))\\w+(?=(\\s\\[))"))
 
+    #Scaling, x = 2.25 and y=1.7, reverse y-axis
     positions <- data.frame(vars = vars,
-                            x = as.numeric(unlist(str_extract_all(dagitty, "(?<=(pos..))\\-?\\d+\\.?\\d*"))[1:length(vars)]),
-                            y = -1*as.numeric(unlist(str_extract_all(dagitty, "(?<=(\\,))\\-?\\d+\\.?\\d*"))[1:length(vars)]))
+                            x = (as.numeric(unlist(str_extract_all(dagitty, "(?<=(pos..))\\-?\\d+\\.?\\d*"))[1:length(vars)])+2.7),
+                            y = (-1*as.numeric(unlist(str_extract_all(dagitty, "(?<=(\\,))\\-?\\d+\\.?\\d*"))[1:length(vars)])+1.5))
 
     from <- unlist(str_extract_all(dagitty, "\\w+(?=(\\s\\-\\>))"))
     to <- unlist(str_extract_all(dagitty, "(?<=(\\-\\>\\s))\\w+"))
@@ -71,10 +111,21 @@ dagR <- function(treatment,
                              rep(seq(3,9),2)),
                       y <- c(rep(c(4,6), each=5),
                              rep(c(3,7),each=7)))
-
+  set.seed(seed)
   positions <- data.frame(vars = vars,
              x = c(5,7,6,sample(slots$x, size = (length(vars)-3))),
              y = c(5,5,4,sample(slots$y, size = (length(vars)-3))))
+
+  }
+
+  #Update positions manually
+  if(!is.null(label.positions)) {
+
+    for(i in names(label.positions)) {
+      positions[positions$vars == i, "x"] <- label.positions[[i]][1]
+      positions[positions$vars == i, "y"] <- label.positions[[i]][2]
+
+    }
 
   }
 
@@ -84,32 +135,82 @@ dagR <- function(treatment,
 
       bind_cols(positions %>%
                   filter(vars %in%  names(path.list)[i]) %>%
-                  select(x,y),
+                  select(vars, x,y) %>%
+                  rename(vars_from = vars),
 
                 positions %>%
-                  filter(vars %in% path.list[[i]]$to) %>%
-                  select(x,y) %>%
-                  rename(xend = x,
-                         yend= y))
+                  filter(vars %in% path.list[[i]]) %>%
+                  select(vars, x,y) %>%
+                  rename(vars_to = vars,
+                         xend = x,
+                         yend= y)) %>%
+        mutate(curvature = 0)
 
-    })) %>%
-    mutate(xend = xend-((xend-x)*arrow.distance),
-           yend = yend-((yend-y)*arrow.distance))
+    }))
 
-  positions %>%
-    mutate(vars = str_to_title(vars)) %>%
-  ggplot(aes(x=x, y=y, label = vars)) +
-    geom_curve(data = segs, aes(x=x,
-                                xend=xend,
-                                y=y,
-                                yend=yend),
-               inherit.aes=F,
-               arrow = arrow(type = "closed"),
-               arrow.fill = arrow.fill,
-               color = arrow.color,
-               linewidth = arrow.width,
-               curvature = curvature) +
-    geom_label(size = label.size, fill = label.fill, border.color = label.color) +
+  degrees <- (pi*seq(0,360,5))/180
+
+  #Add euclidian distance to all points + update evenutal curvatures
+  for(n in 1:nrow(segs)) {
+
+    x <- segs[n, "x"]
+    y <- segs
+
+    xend <- segs[n,"xend"]
+    yend <- segs[n,"yend"]
+
+    xs <- segs[n,"xend"]+cos(degrees)*arrow.distance*1.5
+    ys <- segs[n,"yend"]+sin(degrees)*arrow.distance
+
+    min <- which.min(sqrt((xs-segs[n, "x"])^2 + (ys - segs[n, "y"])^2))
+
+    segs[n, "xend"] <- xs[min]
+    segs[n, "yend"] <- ys[min]
+
+  }
+
+  if(!is.null(curvature)) {
+
+    for(i in seq_along(curvature)) {
+
+      segs$curvature[segs$vars_from == curvature[[i]][1] & segs$vars_to == curvature[[i]][2]] <- curvature[[i]][3]
+
+    }
+
+  }
+
+  segs <- segs %>%
+    mutate(lw = ifelse(vars_from == treatment & vars_to == outcome, arrow.linewidth*2, arrow.linewidth))
+
+  positions <- positions %>%
+    mutate(
+      cols = case_when(vars == treatment ~ treatment.fill,
+                       vars == outcome ~ outcome.fill,
+                       T ~ label.fill),
+      vars = str_to_title(vars))
+
+  p <- ggplot(positions, aes(x=x, y=y, label = vars))
+
+  for(n in 1:nrow(segs)) {
+
+    p <- p +
+      annotate("curve",
+               x=segs[n, "x"],
+               xend = segs[n, "xend"],
+               y=segs[n, "y"],
+               yend=segs[n, "yend"],
+               arrow = arrow(type = "closed",
+                             length = unit(arrow.size, "mm")),
+               linewidth = segs[n, "lw"],
+               curvature = segs[n, "curvature"])
+
+
+  }
+
+  p <- p +
+    geom_label(size = label.size, border.color = label.color, fill = positions$cols) +
     theme_void()
+
+return(p)
 
 }
